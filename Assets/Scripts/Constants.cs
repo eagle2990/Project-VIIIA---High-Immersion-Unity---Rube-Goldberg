752b69d812f6fdc8268305230d15460a3d1ab36e
eagle2990 <eagle2990@gmail.com>
2017-08-07 03:30:01 -0500

    Adding bouncy physic material + fan rotation

--- Assets/Scripts/Constants.cs
+++ Assets/Scripts/Constants.cs
@@ -8,6 +8,10 @@ public class Constants
     {
         public const string GROUND = "Ground";
         public const string THROWABLE = "Throwable";
+        public const string TRAMPOLINE = "Trampoline";
+        public const string TELEPORT = "Teleport";
+        public const string GOAL = "Goal";
+        public const string RAIL = "Rail";
     }
 
     public const float THROW_FORCE = 1.5f;
---
+++ Assets/Scripts/FanRotation.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FanRotation : MonoBehaviour
+{
+    public float initialSpeed = 0.0f;
+    public float maxSpeed = 10.0f;
+    public float acelerator = 1.0f;
+    public string rotationAngle = "forward";
+
+    private float currentSpeed;
+
+    // Use this for initialization
+    void Start()
+    {
+        currentSpeed = initialSpeed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (rotationAngle.Contains("up"))
+            transform.Rotate(Vector3.up, currentSpeed);
+        else if (rotationAngle.Contains("forward"))
+            transform.Rotate(Vector3.forward, currentSpeed);
+        else
+            transform.Rotate(Vector3.right, currentSpeed);
+
+        if(currentSpeed <= maxSpeed)
+        {
+            currentSpeed += Time.deltaTime * acelerator;
+        }
+    }
+
+    public float GetCurrentSpeed()
+    {
+        return currentSpeed;
+    }
+}
